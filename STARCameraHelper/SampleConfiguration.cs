8ce074078aa67f114c19b30526bf68edfd4e18c9
Andersen <andersed@purdue.lcl>
2017-10-09 13:55:26 -0400

    brought over all code from CameraOpenCV sample

---
+++ STARCameraHelper/SampleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace STARCameraHelper
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "STAR Camera Helper";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() { Title="Example Operations", ClassType=typeof(Scenario1_ExampleOperations)},
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+}
